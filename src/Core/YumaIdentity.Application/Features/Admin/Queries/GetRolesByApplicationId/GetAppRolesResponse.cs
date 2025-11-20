f05516d5e22e5b9e24e4407144f89dc420389843
AdylshaY <adylshay@gmail.com>
2025-11-20 23:24:29 +0300

    Add GetRolesByApplicationId query and handler to retrieve roles by application ID

---
+++ src/Core/YumaIdentity.Application/Features/Admin/Queries/GetRolesByApplicationId/GetAppRolesResponse.cs
@@ -0,0 +1,8 @@
+namespace YumaIdentity.Application.Features.Admin.Queries.GetRolesByApplicationId
+{
+  public class GetAppRolesResponse
+  {
+    public Guid RoleId { get; set; }
+    public required string RoleName { get; set; }
+  }
+}
