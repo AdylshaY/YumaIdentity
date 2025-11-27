b52737f6069857bb360b2d2ce0acaad6ec796431
AdylshaY <adylshay@gmail.com>
2025-11-28 00:22:48 +0300

    Add ForgotPassword and ResetPassword functionality

---
+++ backend/src/Core/YumaIdentity.Application/Features/Auth/Commands/ForgotPassword/ForgotPasswordRequest.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System.ComponentModel.DataAnnotations;
+
+namespace YumaIdentity.Application.Features.Auth.Commands.ForgotPassword
+{
+    public class ForgotPasswordRequest : IRequest<Unit>
+    {
+        [Required]
+        [EmailAddress]
+        public required string Email { get; set; }
+
+        public string? ClientId { get; set; }
+        public string? ClientSecret { get; set; }
+    }
+}
