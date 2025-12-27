15b95d253756ce150afe62662635cd5ca92b7642
AdylshaY <adylshay@gmail.com>
2025-12-27 23:33:28 +0300

    feat: refactor frontend structure by removing unused UI components and updating main application layout

--- frontend/apps/web/src/main.tsx
+++ frontend/apps/web/src/main.tsx
@@ -1,25 +1,30 @@
-import { createRoot } from "react-dom/client";
-import "./style.css";
-import typescriptLogo from "/typescript.svg";
-import { Header, Counter } from "@repo/ui";
+import { createRoot } from 'react-dom/client';
+import './style.css';
+import typescriptLogo from '/typescript.svg';
 
 const App = () => (
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" className="logo" alt="Vite logo" />
+    <a href='https://vitejs.dev' target='_blank'>
+      <img src='/vite.svg' className='logo' alt='Vite logo' />
     </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
+    <a href='https://www.typescriptlang.org/' target='_blank'>
       <img
         src={typescriptLogo}
-        className="logo vanilla"
-        alt="TypeScript logo"
+        className='logo vanilla'
+        alt='TypeScript logo'
       />
     </a>
-    <Header title="Web" />
-    <div className="card">
-      <Counter />
+    <h1>Vite + TypeScript</h1>
+    <div className='card'>
+      <button
+        onClick={() => {
+          alert('Hello Vite + TypeScript!');
+        }}
+      >
+        Click me
+      </button>
     </div>
   </div>
 );
 
-createRoot(document.getElementById("app")!).render(<App />);
+createRoot(document.getElementById('app')!).render(<App />);
